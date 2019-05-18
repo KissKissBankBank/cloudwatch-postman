e0170ebfeeb3ab688396fa2b1b38934ec35a249a
Fanny Cheung <fanny.cheung@kisskissbankbank.com>
2019-05-18 23:29:03 +0200

    Add real tests (#5)

--- test/index.js
+++ test/index.js
@@ -1,10 +1,45 @@
 import chai, { expect } from 'chai'
+import chaiHttp from 'chai-http'
+import server from './../lib/index'
 
 global.chai = chai
 global.expect = expect
+chai.use(chaiHttp)
 
-describe('index', () => {
-  it('returns true', () => {
-    expect(true).to.eq(true)
+describe('GET /test', () => {
+  it('returns a response', (done) => {
+    chai.request(server)
+      .get('/test')
+      .end((err, res) => {
+        expect(res.status).to.eq(200)
+        expect(JSON.parse(res.body)).to.be.an('object')
+        expect(JSON.parse(res.body)).to.have.key('ResponseMetadata')
+        done()
+      })
+  })
+})
+
+describe('POST /metric', () => {
+  it('returns a response', (done) => {
+    const params = {
+      MetricData: [
+        {
+          MetricName: 'HELLO_WORLD',
+          Value: 100,
+        }
+      ],
+      Namespace: 'cloudwatch-postman',
+    }
+
+    chai.request(server)
+      .post('/metric')
+      .set('Content-Type', 'application/json')
+      .send(JSON.stringify(params))
+      .end((err, res) => {
+        expect(res.status).to.eq(200)
+        expect(res.body).to.be.an('object')
+        expect(res.body).to.have.key('ResponseMetadata')
+        done()
+      })
   })
 })
