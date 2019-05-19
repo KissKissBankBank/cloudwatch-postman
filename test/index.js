d48769d19d7d462c11ab4282c7dde952c2f85fab
Fanny Cheung <fanny@ynote.hk>
2019-05-19 18:53:15 +0200

    Add app and access token

--- test/index.js
+++ test/index.js
@@ -2,10 +2,26 @@ import chai, { expect } from 'chai'
 import chaiHttp from 'chai-http'
 import server from './../lib/index'
 import AWS from 'aws-sdk-mock'
+import {
+  generateSalt,
+  generateHash,
+  generateToken,
+  createAccessToken,
+} from '../lib/token'
 
 global.chai = chai
 global.expect = expect
 chai.use(chaiHttp)
+
+export const createTestAppToken = () => {
+  const date = new Date().getTime()
+  const salt = generateSalt(8)
+  const secret = process.env.CLOUDWATCH_POSTMAN_SECRET_KEY
+  const delimiter = '::'
+  const hash = generateHash(`${date}${salt}${secret}`)
+
+  return generateToken([date, salt, secret], delimiter)
+}
 
 beforeEach(() => {
   AWS.mock('CloudWatch', 'putMetricData', function (params, callback){
@@ -19,8 +35,9 @@ afterEach(() => {
 
 describe('GET /test', () => {
   it('returns a response', (done) => {
+    const appToken = createTestAppToken()
     chai.request(server)
-      .get('/test')
+      .get(`/test?appToken=${appToken}`)
       .end((err, res) => {
         expect(res.status).to.eq(200)
         expect(JSON.parse(res.body)).to.eq('ok')
@@ -31,14 +48,18 @@ describe('GET /test', () => {
 
 describe('POST /metric', () => {
   it('returns a response', (done) => {
+    const accessToken = createAccessToken()
     const params = {
-      MetricData: [
-        {
-          MetricName: 'HELLO_WORLD',
-          Value: 100,
-        }
-      ],
-      Namespace: 'cloudwatch-postman',
+      accessToken,
+      params: {
+        MetricData: [
+          {
+            MetricName: 'HELLO_WORLD',
+            Value: 100,
+          }
+        ],
+        Namespace: 'cloudwatch-postman',
+      }
     }
 
     chai.request(server)
